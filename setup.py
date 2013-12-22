50d382e152ac25dc6db412d59bc478351a72ef12
Martin Haecker <spamfaenger@gmx.de>
2013-12-22 10:49:19 +0100

    Add setup.py file to enable distribution

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding: utf8
+
+from distutils.core import setup
+
+setup(
+    name='pyexpect',
+    version='1.0',
+    description='Python expectaton library',
+    long_description=file('README.md').readlines(),
+    author='Martin Häcker',
+    author_email='spamfaenger@gmx.de',
+    url='https://bitbucket.org/dwt/pyexpect',
+    py_modules=['pyexpect'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Quality Assurance',
+    ],
+)
