8e61dd4bad40626be987bfc778013c5640410ba0
Martin Haecker <spamfaenger@gmx.de>
2014-12-06 21:39:41 +0100

    Increase version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def readme():
 
 setup(
     name='pyexpect',
-    version='1.0.8',
+    version='1.0.9',
     description='Python expectaton library',
     long_description=readme(),
     author='Martin Häcker',
