5f3719665b5bb6335d29d41bb32018ba57f169c5
Martin Haecker <spamfaenger@gmx.de>
2015-03-21 14:37:20 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def readme():
 
 setup(
     name='pyexpect',
-    version='1.0.10',
+    version='1.0.11',
     description='Python expectaton library',
     long_description=readme(),
     author='Martin Häcker',
