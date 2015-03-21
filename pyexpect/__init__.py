851e3f05717e023456487d08a0c4185742090db8
Martin Haecker <spamfaenger@gmx.de>
2015-03-21 09:10:29 +0100

    Move meta functionality into a superclass for ease of working with it and better separation from the matchers themselves.

--- pyexpect/__init__.py
+++ pyexpect/__init__.py
@@ -20,8 +20,9 @@
 __all__ = ['expect']
 
 import re, sys
+from .internals import ExpectMeta
 
-class expect(object):
+class expect(ExpectMeta):
     """Minimal but very flexible implementation of the expect pattern.
     
     Use like this:
@@ -96,6 +97,7 @@ class expect(object):
     
     ## Compatibility with other expectation packages ####################################################
     
+    # REFACT: make this something that can be mixed in at will when defining your local expect
     @classmethod
     def enable_jasmine_compatibility(cls):
         # defined is not really a concept that maps well to python
@@ -165,7 +167,7 @@ class expect(object):
         self._assert(self._actual is None, "to be None")
     
     is_none = none
-    
+
     def exist(self):
         self._assert(self._actual is not None, "to exist (not be None)")
     
@@ -173,7 +175,7 @@ class expect(object):
     # REFACT: consider adding 'from' alias to allow syntax like expect(False).from(some_longish_expression())
     # Could enhance readability, not sure it's a good idea?
     def equal(self, something):
-        self._assert(something == self._actual, "to equal {0!r}", something)
+        self._assert(something == self._actual, "\nto equal {0!r}", something)
     
     __eq__ = equals = equal
     to_equal = is_equal = equal
@@ -234,7 +236,7 @@ class expect(object):
         actual_items = [(key, self._actual.get(key)) for key in expected_keys]
         # superset = set(self._actual.iteritems())
         # REFACT: subset.issubset(superset)
-        self._assert(actual_items == expected_items, 'to contain dict {0!r}', a_subdict)
+        self._assert(actual_items == expected_items, '\nto contain dict {0!r}', a_subdict)
     
     includes_dict = contains_dict = subdict = sub_dict
     have_subdict = have_sub_dict = has_subdict = has_sub_dict = sub_dict
@@ -372,155 +374,3 @@ class expect(object):
     about_equals = about_equal = about = almost_equals = almost_equal = close = close_to
     is_about =  is_almost_equal = is_close = is_close_to = close_to
     
-    ## Internals ########################################################################################
-    
-    def __getattribute__(self, name):
-        """Allows you to chain any python identifier to this object and keep 
-        chaining for as long as you want as syntactic sugar.
-        
-        Only the last identifier that is then actually called has to be an actual 
-        matcher.
-        
-        Including 'not' in any of the identifiers used in chaining will invert the 
-        whole expectation. This includes calling the negated form of a matcher by 
-        prefixing it with not_ like 'not_to_be' to get the inverted form of 'to_be'.
-        """
-        # Allow normal access to everything that is not a matcher. 
-        # (Every public method is treated as a matcher)
-        # REFACT: I would prefer it if not every access to private methods would go through this method
-        # However as I do not want to return instance methods and other existing methods directly, there
-        # seems to be no way to do this.
-        if name.startswith('_'):
-            return object.__getattribute__(self, name)
-        
-        # If you include not somewhere in the called attributes name, 
-        # switch the expectation to negative.
-        if name.startswith('not_') or '_not_' in name or name.endswith('_not'):
-            self._expected_assertion_result = False
-        
-        self._prepare_matcher_for_calling(name, self._matcher_with_name(name))
-        
-        # Allow arbitrary chaining
-        return self
-    
-    def _matcher_with_name(self, name):
-        # REFACT: consider to change matcher lookup to allow some more pre- and suffixes
-        # could be be_ to_ and some more to dry up the list of alternative names that have 
-        # to be written out for each matcher.
-        if name.startswith('not_'):
-            name = name[4:]
-        
-        # need object.__getattribute__ to prevent recursion with self.__getattribute__
-        try:
-            return object.__getattribute__(self, name)
-        except AttributeError as e:
-            return None
-    
-    def _prepare_matcher_for_calling(self, name, matcher):
-        self._selected_matcher_name = name
-        self._selected_matcher = matcher
-    
-    def __call__(self, *args, **kwargs):
-        """Called whenever you actualy invoke a matcher. 
-        
-        Provides a good error message if you mistype the matcher name.
-        
-        Supports custom messages with the message keyword argument"""
-        __tracebackhide__ = True  # Hide from py.test tracebacks
-        
-        if self._selected_matcher is None:
-            # REFACT: consider raising NotImplementedError
-            raise AssertionError("Tried to call non existing matcher '{0}' (Patches welcome!)".format(self._selected_matcher_name))
-        
-        try:
-            return_value = self._selected_matcher(*args, **kwargs)
-            # allow an otherwise raising matcher to return something
-            # usefull for matchers like expect.to_raise() to return the caught exception for further analysis
-            if self._should_raise:
-                return return_value
-        except AssertionError as assertion:
-            message = self._message(assertion)
-            
-            if self._should_raise:
-                raise AssertionError(message)
-                # Make the stacktrace easier to read by tricking python to shorten the stack trace to this method.
-                # Hides the actual matcher and all the methods it calls to assert stuff.
-            
-            return (False, message)
-        
-        return (True, "")
-    
-    def _assert(self, assertion, message_format, *message_positionals, **message_keywords):
-        assert assertion is self._expected_assertion_result, \
-            message_format.format(*message_positionals, **message_keywords)
-    
-    def _assert_if_positive(self, assertion, message_format, *message_positionals, **message_keywords):
-        if self._is_negative():
-            return
-        
-        self._assert(assertion, message_format, *message_positionals, **message_keywords)
-    
-    def _assert_if_negative(self, assertion, message_format, *message_positionals, **message_keywords):
-        if not self._is_negative():
-            return
-        
-        self._assert(assertion, message_format, *message_positionals, **message_keywords)
-    
-    def _message(self, assertion):
-        message = self._message_from_assertion(assertion)
-        actual = self._actual
-        optional_negation = ' not ' if self._is_negative() else ' '
-        assertion_message = "Expect {actual!r}{optional_negation}{message}".format(**locals())
-        
-        if self._custom_message is not None:
-            return self._custom_message.format(**locals())
-        
-        return assertion_message
-    
-    def _message_from_assertion(self, assertion):
-        if sys.version < '3':
-            try: return unicode(assertion).encode('utf8')
-            except UnicodeDecodeError as ignored: pass
-        
-        return str(assertion)
-    
-    def _is_negative(self):
-        return self._expected_assertion_result is False
-    
-    def _concatenate(self, *args):
-        return args
-    
-    @classmethod
-    def _enable_nicer_backtraces_for_new_double_underscore_matcher_alternatives(cls):
-        """Works around the problem that special methods (as in '__something__') are not resolved
-        using __getattribute__() and thus do not provide the nice tracebacks that they public matchers provide.
-        
-        This method works on the class dict and is repeated after each instantiation
-        to support adding or overwriting existing matchers at any time while not repeatedly doing the same.
-        """
-        # TODO: try if turning the special methods into descriptors allows to trigger
-        # self.__getattribute__() without increasing the stack trace length on failures
-        def wrap(special_method_name, public_name):
-            def wrapper(self, *args, **kwargs):
-                __tracebackhide__ = True  # Hide from py.test tracebacks
-                # Sadly this increases the traceback lenght by one entry for other testing frameworks :/
-                self.__getattribute__(public_name)(*args, **kwargs)
-            setattr(cls, special_method_name, wrapper)
-        
-        special_names = filter(lambda name: name.startswith('__') and name.endswith('__'), dir(cls))
-        # Assumes that all public methods are matchers
-        matcher_names = filter(lambda name: not name.startswith('_'), dir(cls))
-        public_matcher_names_by_matcher = dict()
-        for name in matcher_names:
-            matcher = getattr(cls, name)
-            public_matcher_names_by_matcher[matcher] = name
-        matchers = list(public_matcher_names_by_matcher.keys())
-        
-        for special_name in special_names:
-            matcher = getattr(cls, special_name)
-            if matcher in matchers:
-                public_name = public_matcher_names_by_matcher[matcher]
-                wrap(special_name, public_name)
-    
-
-
---
+++ pyexpect/internals.py
@@ -0,0 +1,190 @@
+import sys
+
+class ExpectMeta(object):
+    
+    ## Internals ########################################################################################
+    # REFACT: consider to find a way to separate them better from the matchers. Maybe move to a different
+    # file and include as a superclass? Could even access the attributes via the class object directly to get rid of much of the __getattribute__ hack?
+    
+    def __getattribute__(self, name):
+        """Allows you to chain any python identifier to this object and keep 
+        chaining for as long as you want as syntactic sugar.
+        
+        Only the last identifier that is then actually called has to be an actual 
+        matcher.
+        
+        Including 'not' in any of the identifiers used in chaining will invert the 
+        whole expectation. This includes calling the negated form of a matcher by 
+        prefixing it with not_ like 'not_to_be' to get the inverted form of 'to_be'.
+        """
+        # Allow normal access to everything that is not a matcher. 
+        # (Every public method is treated as a matcher)
+        # REFACT: I would prefer it if not every access to private methods would go through this method
+        # However as I do not want to return instance methods and other existing methods directly, there
+        # seems to be no way to do this.
+        if name.startswith('_'):
+            return object.__getattribute__(self, name)
+        
+        # If you include not somewhere in the called attributes name, 
+        # switch the expectation to negative.
+        if name.startswith('not_') or '_not_' in name or name.endswith('_not'):
+            self._expected_assertion_result = False
+        
+        self._prepare_matcher_for_calling(name, self._matcher_with_name(name))
+        
+        # Allow arbitrary chaining
+        return self
+    
+    def _matcher_with_name(self, name):
+        # REFACT: consider to change matcher lookup to allow some more pre- and suffixes
+        # could be be_ to_ and some more to dry up the list of alternative names that have 
+        # to be written out for each matcher.
+        if name.startswith('not_'):
+            name = name[4:]
+        
+        # need object.__getattribute__ to prevent recursion with self.__getattribute__
+        try:
+            return object.__getattribute__(self, name)
+        except AttributeError as e:
+            return None
+    
+    def _prepare_matcher_for_calling(self, name, matcher):
+        self._selected_matcher_name = name
+        self._selected_matcher = matcher
+    
+    def __call__(self, *args, **kwargs):
+        """Called whenever you actualy invoke a matcher. 
+        
+        Provides a good error message if you mistype the matcher name.
+        
+        Supports custom messages with the message keyword argument"""
+        __tracebackhide__ = True  # Hide from py.test tracebacks
+        
+        if self._selected_matcher is None:
+            # REFACT: consider raising NotImplementedError
+            raise AssertionError("Tried to call non existing matcher '{0}' (Patches welcome!)".format(self._selected_matcher_name))
+        
+        try:
+            return_value = self._selected_matcher(*args, **kwargs)
+            # allow an otherwise raising matcher to return something
+            # usefull for matchers like expect.to_raise() to return the caught exception for further analysis
+            if self._should_raise:
+                return return_value
+        except AssertionError as assertion:
+            message = self._message(assertion)
+            
+            if self._should_raise:
+                # _, _, exception_traceback = sys.exc_info()
+                # exception_traceback = self._traceback_with_last_call_before_expect_on_stack()
+                # exception_traceback = None
+                # exc = AssertionError(message)
+                # exc.with_traceback(None)
+                # exc.__cause__ = None
+                # raise exc
+                self._raise_with_traceback(AssertionError(message), traceback=None, cause_exception=None)
+                # Make the stacktrace easier to read by tricking python to shorten the stack trace to this method.
+                # Hides the actual matcher and all the methods it calls to assert stuff.
+            
+            return (False, message)
+        
+        return (True, "")
+    
+    
+    def _raise_with_traceback(self, exc, traceback=Ellipsis, cause_exception=None):
+        """Emulates python 3's
+            raise some_exception.with_traceback(a_custom_traceback) from cause_exception
+        """
+        PY2 = sys.version_info[0] == 2
+        if PY2:
+            exec('''
+def raise_with_traceback(exc, traceback=Ellipsis, cause_exception=None):
+    if traceback == Ellipsis:
+        _, _, traceback = sys.exc_info()
+    raise exc, None, traceback
+self._raise_with_traceback = raise_with_traceback
+'''.strip())
+        else:
+            exec('''
+def raise_with_traceback(exc, traceback=Ellipsis, cause_exception=None):
+    if traceback == Ellipsis:
+        _, _, traceback = sys.exc_info()
+    exc.with_traceback(traceback).__cause__ = cause_exception
+    raise exc
+self._raise_with_traceback = raise_with_traceback
+'''.strip())
+        
+        self._raise_with_traceback(exc, traceback, cause_exception)
+    
+    def _assert(self, assertion, message_format, *message_positionals, **message_keywords):
+        assert assertion is self._expected_assertion_result, \
+            message_format.format(*message_positionals, **message_keywords)
+    
+    def _assert_if_positive(self, assertion, message_format, *message_positionals, **message_keywords):
+        if self._is_negative():
+            return
+        
+        self._assert(assertion, message_format, *message_positionals, **message_keywords)
+    
+    def _assert_if_negative(self, assertion, message_format, *message_positionals, **message_keywords):
+        if not self._is_negative():
+            return
+        
+        self._assert(assertion, message_format, *message_positionals, **message_keywords)
+    
+    def _message(self, assertion):
+        message = self._message_from_assertion(assertion)
+        actual = self._actual
+        optional_negation = ' not ' if self._is_negative() else ' '
+        assertion_message = "Expect {actual!r}{optional_negation}{message}".format(**locals())
+        
+        if self._custom_message is not None:
+            return self._custom_message.format(**locals())
+        
+        return assertion_message
+    
+    def _message_from_assertion(self, assertion):
+        if sys.version < '3':
+            try: return unicode(assertion).encode('utf8')
+            except UnicodeDecodeError as ignored: pass
+        
+        return str(assertion)
+    
+    def _is_negative(self):
+        return self._expected_assertion_result is False
+    
+    def _concatenate(self, *args):
+        return args
+    
+    @classmethod
+    def _enable_nicer_backtraces_for_new_double_underscore_matcher_alternatives(cls):
+        """Works around the problem that special methods (as in '__something__') are not resolved
+        using __getattribute__() and thus do not provide the nice tracebacks that they public matchers provide.
+        
+        This method works on the class dict and is repeated after each instantiation
+        to support adding or overwriting existing matchers at any time while not repeatedly doing the same.
+        """
+        # TODO: try if turning the special methods into descriptors allows to trigger
+        # self.__getattribute__() without increasing the stack trace length on failures
+        def wrap(special_method_name, public_name):
+            def wrapper(self, *args, **kwargs):
+                __tracebackhide__ = True  # Hide from py.test tracebacks
+                # Sadly this increases the traceback lenght by one entry for other testing frameworks :/
+                self.__getattribute__(public_name)(*args, **kwargs)
+            setattr(cls, special_method_name, wrapper)
+        
+        special_names = filter(lambda name: name.startswith('__') and name.endswith('__'), dir(cls))
+        # Assumes that all public methods are matchers
+        matcher_names = filter(lambda name: not name.startswith('_'), dir(cls))
+        public_matcher_names_by_matcher = dict()
+        for name in matcher_names:
+            matcher = getattr(cls, name)
+            public_matcher_names_by_matcher[matcher] = name
+        matchers = list(public_matcher_names_by_matcher.keys())
+        
+        for special_name in special_names:
+            matcher = getattr(cls, special_name)
+            if matcher in matchers:
+                public_name = public_matcher_names_by_matcher[matcher]
+                wrap(special_name, public_name)
+    
+
