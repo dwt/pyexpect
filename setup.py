3f1d2828e2f53135f2e82e2b86e0bd73056402e1
Martin Haecker <spamfaenger@gmx.de>
2016-02-26 14:50:48 +0100

    Update to 1.0.15

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def readme():
 
 setup(
     name='pyexpect',
-    version='1.0.14',
+    version='1.0.15',
     description='Python expectaton library',
     long_description=readme(),
     author='Martin Häcker',
