e081caa1901107a1b2432f814d394e49e63efeb1
Martin Haecker <spamfaenger@gmx.de>
2017-02-16 11:06:25 +0100

    - current modifications -

--- setup.py
+++ setup.py
@@ -2,17 +2,20 @@
 # encoding: utf8
 
 from setuptools import setup, find_packages
+import os
+
+here = os.path.dirname(os.path.abspath(__file__))
 
 def readme():
     "Falls back to just file().read() on any error, because the conversion to rst is only really relevant when uploading the package to pypi"
     from subprocess import CalledProcessError
     try:
         from subprocess import check_output
-        return str(check_output(['pandoc', '--from', 'markdown', '--to', 'rst', 'README.md']))
+        return str(check_output(['pandoc', '--from', 'markdown', '--to', 'rst', os.path.join(here, 'README.md')]))
     except (ImportError, OSError, CalledProcessError) as error:
         print('python2.6 and pandoc is required to get the description as rst (as required to get nice rendering in pypi) - using the original markdown instead.',
               'See http://johnmacfarlane.net/pandoc/')
-    return str(open('README.md').read())
+    return str(open(os.path.join(here, 'README.md')).read())
 
 setup(
     name='pyexpect',
@@ -24,6 +27,7 @@ setup(
     license='ISC',
     url='https://bitbucket.org/dwt/pyexpect',
     packages=find_packages(),
+    include_package_data=True,
     test_suite="tests",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
